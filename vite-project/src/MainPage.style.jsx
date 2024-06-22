9788f52802b13466fbb62391a41bc9d636d09886
garden <wunsoho@naver.com>
2024-06-22 15:24:37 +0900

    5주차 완료

--- vite-project/src/MainPage.style.jsx
+++ vite-project/src/MainPage.style.jsx
@@ -22,12 +22,90 @@ export const Search = styled.div`
   .movieimg{
     border-radius : 5vw;
   }
+  .search1{
+    padding-left : 1vw;
+    margin-top : 5vh;
+    width : 23vw;
+    height : 3.5vh;
+    border-radius : 5vw;
+  }
 `
 export const Title = styled.div`
   display : flex;
   align-items : center;
   justify-content : center;
   .title1{
-    margin-left : 1vw;
+    margin-left : 1vw;  
   }
 `
+export const Info = styled.div`
+  background-color : #11264f;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 5vh;
+  width: 70vw;
+  max-height: 100vh;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 5px;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`
+
+export const Component = styled.div`
+    margin-left : 2vw;
+    margin-top : 2vh;
+    background-color: #373b69;
+    position: relative;
+    display:inline-block;
+`;
+
+export const Movieimg = styled.div`
+    width: 200px;
+    background-color: #373b69;
+    position: relative;
+`;
+export const Img = styled.img`
+  width: 100%;
+`;
+export const Movieinfo = styled.div`
+  font-size : 0.5em;
+  margin-left: 1vw;
+  margin-top: 0.5vh;
+  margin-bottom : 3vh;
+  color: #FFFFFF;
+  display:flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  #average {
+    margin-right: 1vw;
+    white-space: nowrap;
+  }
+  .title {
+    flex: 1;
+    margin-right: 1vw;
+  }
+`;
+export const Hideinfo = styled.div`
+    display: block;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    color: #c7c7c7;
+    padding-left: 5px;
+    padding-right: 5px;
+    font-size: 13px;
+    height: 100%;
+    border-radius: 0 0 5px 5px;
+    background-color: rgba(0, 0, 0, 0.7);
+    overflow: hidden;
+    text-overflow: ellipsis;
+`;
--- vite-project/src/components/MainPage.jsx
+++ vite-project/src/components/MainPage.jsx
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import * as B from '../MainPage.style.jsx'
 import Slider from 'react-slick';
 import movieImg from '../movie1.png';
-
 
 function MainPage() {
   const settings1 = {
@@ -11,6 +12,34 @@ function MainPage() {
     slidesToScroll: 1,
     arrows:false,
 };
+const domain = "https://image.tmdb.org/t/p/w1280/";
+const [search, setSearch] = useState([]);
+const [title, setTitle] = useState('');
+const [hoveredIndex, setHoveredIndex] = useState(null);
+
+useEffect(() => {
+  const fetchData = async () => {
+    if (title.trim() !== '') {
+      try {
+        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=true&language=en-US&page=1`, {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOGY2YmE1ZmUwZTRkN2U0ZGI1Mjc5M2U1Y2ViZTE4ZiIsInN1YiI6IjY2Mjg5NmM0MTc2YTk0MDE2NjgyMjliMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sl80DdobF9_NA9Mib8y9iGb54XIqgHu6igfwZjFfP8I`
+          },
+        });
+        setSearch(response.data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  };
+  fetchData();
+}, [title]);
+
+const handleInputChange = (e) => {
+  setTitle(e.target.value)
+}
     return (
       <div>
         <B.CustomSlider>
@@ -25,8 +54,27 @@ function MainPage() {
             <img className="movieimg"src={movieImg} width='50px'></img>
             <div className="title1">Find your movies !</div>
           </B.Title>
-          <input></input>
+          <input className="search1"onChange = {handleInputChange}></input>
         </B.Search>
+        {title && (
+        <B.Info>
+          {search.map((movie, index) => ( 
+          <B.Component key={index} onMouseOver={() => setHoveredIndex(index)} onMouseOut={() => setHoveredIndex(null)}>
+            <B.Movieimg>
+              <B.Img src={domain + movie.poster_path} alt={movie.title} />
+              <B.Movieinfo>
+                <div className="title">{movie.title}</div>
+                <div id = "average">{movie.vote_average}</div>
+              </B.Movieinfo>
+            </B.Movieimg>
+            <B.Hideinfo style={hoveredIndex === index ? { display : "block"} : { display : "none"}}>
+              <div>{movie.title}</div>
+              <div>{movie.overview}</div>
+            </B.Hideinfo>
+          </B.Component>
+          ))}
+        </B.Info>
+      )}
       </div>
   );
 }
